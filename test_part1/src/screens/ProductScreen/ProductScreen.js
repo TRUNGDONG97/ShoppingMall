8a7ad1a7be820e1fee05b6af1d7e1106a20fe04e
Jack <tvngoc1@cmc.com.vn>
2021-12-12 21:04:19 +0700

    add react native firebase

--- test_part1/src/screens/ProductScreen/ProductScreen.js
+++ test_part1/src/screens/ProductScreen/ProductScreen.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import {firebase} from '@react-native-firebase/database';
 
 const ProductScreen = () => {
+  useEffect(() => {
+    const database = firebase
+      .app()
+      .database(
+        'https://test-2fbee-default-rtdb.europe-west1.firebasedatabase.app/',
+      )
+      .ref('/product');
+    console.log('database');
+    return () => {};
+  }, []);
   return (
     <View
       style={{
