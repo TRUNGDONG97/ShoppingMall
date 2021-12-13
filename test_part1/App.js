fdb7aeb9dbeb3104cda2025d8692c047b0717e60
Jack <tvngoc1@cmc.com.vn>
2021-12-13 11:41:02 +0700

    refactor code

--- test_part1/App.js
+++ test_part1/App.js
@@ -11,16 +11,6 @@ import type {Node} from 'react';
 import {SafeAreaView, StatusBar, Platform} from 'react-native';
 
 import MainNavigation from './src/navigation/MainNavigation';
-import {iosConfig, androidConfig} from './src/constants/Constant';
-import firebase from '@react-native-firebase/app';
-
-// Your web app's Firebase configuration
-const firebaseConfig = Platform.OS === 'ios' ? iosConfig : androidConfig;
-
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 
 const App: () => Node = () => {
   return (
